3cce106dc60d038690f56590ec095838ffa5bbb6
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-09-30 16:58:31 +0100

    Update LoggerUtil.java

--- src/main/java/pedsim/utilities/LoggerUtil.java
+++ src/main/java/pedsim/utilities/LoggerUtil.java
@@ -1,14 +1,16 @@
 package pedsim.utilities;
 
+import java.awt.TextArea;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class LoggerUtil {
 
-  // Define the SimpleFormatter as a static nested class
+  // --- Simple formatter (console only) ---
   public static class SimpleFormatter extends Formatter {
     @Override
     public String format(LogRecord record) {
@@ -25,17 +27,40 @@ public class LoggerUtil {
       rootLogger.removeHandler(handler);
     }
 
-    // Create a new console handler with the custom formatter
+    // Console handler with custom formatter
     ConsoleHandler handler = new ConsoleHandler();
     handler.setFormatter(new SimpleFormatter());
     rootLogger.addHandler(handler);
 
-    // Set logger level
+    // Set log level
     rootLogger.setLevel(Level.INFO);
     logger.setLevel(Level.INFO);
   }
 
   public static Logger getLogger() {
     return logger;
+  }
+
+  /**
+   * Redirect logger output also to a TextArea in the GUI.
+   */
+  public static void redirectToTextArea(TextArea textArea) {
+    Handler guiHandler = new Handler() {
+      @Override
+      public void publish(LogRecord record) {
+        if (textArea != null && isLoggable(record)) {
+          textArea.append(record.getLevel() + ": " + record.getMessage() + "\n");
+        }
+      }
+
+      @Override
+      public void flush() {}
+
+      @Override
+      public void close() throws SecurityException {}
+    };
+    guiHandler.setLevel(Level.ALL);
+
+    logger.addHandler(guiHandler);
   }
 }
